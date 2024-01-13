f267da897c910deb79b02e32723050d98e690c5d
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2024-01-13 23:11:31 +0000

    changed url parsing

--- svelte-kit-poke-app/src/routes/+page.server.ts
+++ svelte-kit-poke-app/src/routes/+page.server.ts
@@ -5,9 +5,9 @@ export const actions: Actions = {
     uploadResult: async (request) => {
       let split_url = request.url.href.split("?")[1].split("&");
       console.log(split_url);
-      const left_pokemon_name = split_url[1].split("=")[1];
-      const right_pokemon_name = split_url[2].split("=")[1];
-      const who_won = split_url[3].split("=")[1];
+      const left_pokemon_name = split_url[2].split("=")[1];
+      const right_pokemon_name = split_url[1].split("=")[1];
+      const who_won = split_url[0].split("=")[1];
       console.log(left_pokemon_name, right_pokemon_name, who_won);
       query(`insert into pokemon_rating (left_pokemon_name, right_pokemon_name, who_won) values ("${left_pokemon_name}","${right_pokemon_name}","${who_won}")`);
       return {
